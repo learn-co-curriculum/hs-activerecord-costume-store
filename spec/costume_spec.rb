addbc43aa6402864910dd897f10867c21f5b7fc5
Ian Candy <iancandysemail@gmail.com>
2015-09-08 14:27:11 -0400

    shorten attributes and specs

--- spec/costume_spec.rb
+++ spec/costume_spec.rb
@@ -17,15 +17,6 @@ describe "Costume" do
     expect(Costume.find_by(price: 169.97)).to eq(pirate)
   end
 
-  it "has an image url" do
-    url = "http://img.costumecraze.com/images/vendors/rasta/7139-large.jpg"
-    penguin = Costume.create(
-      name: "Happy Penguin",
-      image_url: url
-    )
-    expect(Costume.find_by(image_url: url)).to eq(penguin)
-  end
-
   it "has a size" do
     horse = Costume.create(
       name: "Horse Mask",
@@ -33,20 +24,6 @@ describe "Costume" do
       size: "medium"
     )
     expect(Costume.find_by(size: "medium")).to eq(horse)
-  end
-
-  it "knows when it was created" do
-    bee = Costume.create(name: "Dog's Bumble Bee", image_url: "http://cdn.sheknows.com/filter/l/gallery/halloween_costume_dog_bumblebee.jpg",size: "medium")
-    expect { bee.created_at }.to_not raise_error
-    expect(bee.created_at.to_datetime === Time.now.utc.to_datetime).to eq(true)
-  end
-
-  it "knows when it was last updated" do
-    stegosaurus = Costume.create(name: "Stegosaurus Tortoise Cozy", image_url: "https://img1.etsystatic.com/036/1/7507736/il_570xN.513886615_45eg.jpg")
-    stegosaurus.update(:size => "large")
-    expect { stegosaurus.updated_at }.to_not raise_error
-    expect(stegosaurus.updated_at.to_datetime ===  Time.now.utc.to_datetime).to eq(true)
-    expect(stegosaurus.updated_at.to_datetime).to be > stegosaurus.created_at.to_datetime
   end
 
 end
